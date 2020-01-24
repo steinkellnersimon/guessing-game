7642f743e0664926c6ab957e0d2fca7ba29ec5c6
steinkellnersimon <simon.steinkellner@sz-ybbs.ac.at>
2020-01-24 15:09:39 +0100

    set up command line runner and database infos

--- src/main/java/at/stnwtr/guessinggame/GuessingGameApplication.java
+++ src/main/java/at/stnwtr/guessinggame/GuessingGameApplication.java
@@ -1,13 +1,18 @@
 package at.stnwtr.guessinggame;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GuessingGameApplication {
+public class GuessingGameApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuessingGameApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		// Entry Point
+	}
 }
